c4cab909b205271fe5ea643f7587a6f60af93f15
Kevin Decena <storm_iz_here@yahoo.com>
2025-04-27 20:19:13 +0800

    remove unnecessary slug logic functions

--- app/Filament/Resources/AnnouncementResource/Pages/CreateAnnouncement.php
+++ app/Filament/Resources/AnnouncementResource/Pages/CreateAnnouncement.php
@@ -10,13 +10,6 @@ class CreateAnnouncement extends CreateRecord
 {
     protected static string $resource = AnnouncementResource::class;
 
-    protected function afterCreate(): void
-    {
-        $announcement = $this->getRecord();
-        $announcement->slug = $announcement->id. '-' .$announcement->slug;
-        $announcement->save();
-    }
-
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['user_id'] = Auth::user()->id;
